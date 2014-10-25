50d1dca736e219d0430b07e1a906319e78a0c9b2
Vikraman Choudhury <git@vikraman.org>
2014-10-25 19:00:39 +0530

    Rewrite helper scripts to fit app

--- src/Package.hs
+++ src/Package.hs
@@ -1,19 +1,23 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings  #-}
 
-module Main where
+module Package where
 
 import           Control.Applicative
 import           Control.Monad
 import qualified Data.Attoparsec.Text    as P
 import qualified Data.List               as L
 import qualified Data.Text               as T
+import           Data.Typeable
 import qualified Distribution.Hackage.DB as H
 import qualified Distribution.Version    as V
 import           Github.GitData.Trees    (GitTree (..), nestedTree,
                                           treeGitTrees)
 
-type Tree = [T.Text]
 type Path = T.Text
+type Tree = [T.Text]
+
+type Hackage = H.Map String [V.Version]
 
 getTree :: IO Tree
 getTree = do eTree <- nestedTree "gentoo-haskell" "gentoo-haskell" "master"
@@ -31,7 +35,7 @@ data Ebuild = Ebuild { ebuildCategory :: T.Text
 
 data Package = Package { packageName    :: T.Text
                        , packageVersion :: V.Version
-                       } deriving (Show, Eq)
+                       } deriving (Show, Eq, Ord, Typeable)
 
 parseEbuildVersion :: P.Parser T.Text
 parseEbuildVersion = do chars <- P.manyTill P.anyChar (void ".ebuild" <|> P.endOfInput)
@@ -67,27 +71,40 @@ packagesInEbuilds = mapM packageInEbuild
 packagesInTree :: Tree -> Either String [Package]
 packagesInTree = (packagesInEbuilds =<<) . ebuildsInTree
 
+notLive :: Package -> Bool
+notLive (Package _ (V.Version branches _)) = branches < [9,9,9,9]
+
+latest :: [Package] -> [Package]
+latest ps = maximum <$> L.groupBy (\x y -> packageName x == packageName y) ps
+
 data PackageStatus = Old | New | Unknown deriving (Show, Eq, Ord)
 
-packageStatus :: H.Hackage -> Package -> PackageStatus
+hackageVersion :: Hackage -> Package -> V.Version
+hackageVersion hackage package =
+  case H.lookup (T.unpack . packageName $ package) hackage of
+    Just m ->  maximum m
+    Nothing -> packageVersion package
+
+packageStatus :: Hackage -> Package -> PackageStatus
 packageStatus hackage package =
   case H.lookup (T.unpack . packageName $ package) hackage of
-    Just m -> if L.any (packageVersion package <) (H.keys m)
+    Just m -> if L.any (packageVersion package <) m
                 then Old
                 else New
     Nothing -> Unknown
 
-render :: H.Hackage -> Package -> IO ()
-render h p@(Package name version) = do putStr . show $ name
-                                       putStr " -> "
-                                       putStr . showVersion $ version
-                                       print $ " -> " ++ show (packageStatus h p)
-
 showVersion :: V.Version -> String
 showVersion (V.Version vs _) = L.foldl1 (\a b -> a ++ "." ++ b) $ show `fmap` vs
 
-main :: IO ()
-main = do tree <- getTree
-          hackage <- H.readHackage
-          packages <- either fail return $ packagesInTree tree
-          forM_ packages $ render hackage
+buildHackage :: H.Hackage -> Hackage
+buildHackage = H.map H.keys
+
+readHackage :: IO Hackage
+readHackage = buildHackage <$> H.readHackage
+
+readPackages :: IO [Package]
+readPackages = do tree <- getTree
+                  packages <- either fail return $ packagesInTree tree
+                  let notLivePackages = filter notLive packages
+                      latestPackages = latest notLivePackages
+                  return latestPackages
